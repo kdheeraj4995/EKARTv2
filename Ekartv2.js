8ecf92ce8b8a8c3ccb1de063f409ca1fc35a61d5
Dheeraj Reddy <kdheeraj4995@gmail.com>
2017-07-06 20:43:06 +0530

    Basic Server with Express Js

---
+++ Ekartv2.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+
+
+
+
+
+
+
+
+
+
+// ServerConfig - Start : Running Server on Port 3000 
+app.set('port', 3000);
+var server = app.listen(app.get('port'), function() {
+var port = server.address().port;
+  console.log('Ekartv2 server is running on port ' + port);
+});
+// ServerConfig - End
