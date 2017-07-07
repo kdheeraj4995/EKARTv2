8c78d3147f8006b4c6540e6eda62b37110a05656
Dheeraj Reddy <kdheeraj4995@gmail.com>
2017-07-07 07:57:28 +0530

    Mongodb connection through mongoose

--- Ekartv2.js
+++ Ekartv2.js
@@ -1,15 +1,6 @@
+require('./api/database/dbConnection');
 var express = require('express');
 var app = express();
-
-
-
-
-
-
-
-
-
-
 
 // ServerConfig - Start : Running Server on Port 3000 
 app.set('port', 3000);
@@ -17,4 +8,4 @@ var server = app.listen(app.get('port'), function() {
 var port = server.address().port;
   console.log('Ekartv2 server is running on port ' + port);
 });
-// ServerConfig - End
+// ServerConfig - End 
---
+++ api/database/dbConnection.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var dbUrl = 'mongodb://localhost:27017/Ekartv2';
+
+mongoose.connect(dbUrl, { useMongoClient: true });
+
+
+// Events to capture Mongoose connection status :
+mongoose.connection.on('connected',function(){
+    console.log("Mongoose connected to : "+dbUrl);
+});
+mongoose.connection.on('disconnected',function(){
+    console.log("Mongoose disconnected");
+});
+mongoose.connection.on('error',function(error){
+    console.log("Mongoose error : "+error);
+});
+process.on('SIGINT',function(){
+    mongoose.connection.close(function(){
+        console.log("Reason : Ekartv2 Terminated ")
+        process.exit(0);
+    })
+})
